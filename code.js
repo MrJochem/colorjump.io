065c66d1774d82cec1cf669ae5cb7c1652df16b2
MrJochem <61832710+MrJochem@users.noreply.github.com>
2020-03-12 11:04:30 +0100

    12/3 commit 1

--- code.js
+++ code.js
@@ -37,18 +37,18 @@ function setup() {
         canvas.style('width : auto');
     }
     canvas.id("canvas");
-    document.getElementById("canvas").ontouchstart = function() {
+    document.getElementById("canvas").addEventListener("ontouchdown", function() {
         if (event.type != 'touchstart') return true
         if (gamestate === 1) {
             player.up();
         }
-    }
-    document.getElementById("canvas").onmousedown = function() {
-        if (event.type != 'touchstart') return true
+    }, false)
+    document.getElementById("canvas").addEventListener("mousedown", function() {
+        // if (event.type != 'touchstart') return true
         if (gamestate === 1) {
             player.up();
         }
-    }
+    }, false)
 }
 // player class, dit is de speler, deze heeft een aantal waardes die bij de speler horen in de constructor, ook heeft deze een show functie die de speler laat zien,
 // een update functie die de speler naar beneden laat vallen, een up functie om de speler omhoog te duwen, en een aantal functies om te berekenen of de speler een ander object raakt.
